8ccd2c3f834dcec1e5af49db83ddc43d4527d79c
minseoky <minseok184@gmail.com>
2023-01-22 13:02:22 +0900

    all new

--- src/App.js
+++ src/App.js
@@ -1,150 +1,163 @@
-import { useState, useEffect } from 'react';
-import './body.css'
-let randomIndex;
-const imgs = [
-    'url("https://images.wallpaperscraft.com/image/single/buildings_sea_embankment_669650_3840x2400.jpg")',
-    'url("https://images.wallpaperscraft.com/image/single/buildings_sea_coast_351843_3840x2400.jpg")',
-    'url("https://images.wallpaperscraft.com/image/single/buildings_houses_coast_267033_3840x2400.jpg")'
-];
-randomIndex = Math.floor(Math.random() * imgs.length);
+import styles from './style.module.css'
+import {useState, useEffect} from "react";
+import Swal from "sweetalert2";
 
-function App() {
-    const [todo,setTodo] = useState("");
-    const [todos,setTodos] = useState([]);
-    const [dones,setDones] = useState([]);
-    const [btnColor, setBtnColor] = useState("tomato");
-    const [time, setTime] = useState("morning");
-    const [loggedin, setLoggedin] = useState(localStorage.getItem("logged"));
-    const [clock, setClock] = useState(new Date().toLocaleString())
 
-    setInterval(() => {
-        setClock(new Date().toLocaleString());
-    }, 1000);
+const bgImgs = ["url('https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2144&q=80')",
+    "url('https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')",
+    "url('https://images.unsplash.com/photo-1513407030348-c983a97b98d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2072&q=80')"
+    ]
+let bgIndex = Math.floor(Math.random()*3);
+function Clock(props) {
     useEffect(() => {
-        document.querySelector("title").innerHTML = "To Do List";
-    }, [])
-    const onChange = (e) => {
-        setTodo(e.target.value);
+        clock();
+        modDayTime();
+        },[])
+    const clock = () => {
+        setInterval(() => {
+            props.setTimes([new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()]);
+            clock();
+        }, 1000);
     }
+    useEffect(() => {
+        if(props.dayTime != ""){
+            modDayTime();
+        }
+    }, [props.times[0]])
+    const modDayTime = () => {
+        if(0 < props.times[0] && props.times[0] <= 6){
+            props.setDayTime("새벽");
+        }
+        else if(6< props.times[0] && props.times[0] <= 12){
+            props.setDayTime("아침");
+        }
+        else if(12< props.times[0] && props.times[0] <= 18){
+            props.setDayTime("저녁");
+        }
+        else{
+            props.setDayTime("밤");
+        }
+    }
+    return(
+        <div className={styles.clock}>
+            {String(props.times[0]).padStart(2,'0')}:{String(props.times[1]).padStart(2,'0')}:{String(props.times[2]).padStart(2,'0')}
+        </div>
+    )
+}
+function LoginForm(props) {
+    const [name, setName] = useState("");
+    const onChange = (event) => {
+        setName(event.target.value);
+    }
+    const loginSubmit = (event) => {
+        if(name.length < 6){
+            event.preventDefault();
+            Swal.fire({icon:'success',title:'Welcome.',text:name});
+            localStorage.setItem("username", name);
+            props.setUsername(name);
+        }
+
+        else{
+            event.preventDefault();
+            Swal.fire({icon:'error',title:'Your name is too long.',text:"Try again within 5 characters."});
+            setName("");
+        }
+    }
+    return(
+        <div className={styles.mainContainer}>
+            <form onSubmit={loginSubmit}>
+                <span className={styles.maintext}>좋은 {props.dayTime}입니다. </span>
+                <input className={styles.maintextInput} type={"text"} value={name} onChange={onChange} placeholder={"Input your name"}></input>
+            </form>
+        </div>
+    )
+}
+function AfterLogin(props) {
+    const logout = () => {
+        localStorage.removeItem("username");
+        Swal.fire({
+            title: 'Are you sure to log out?',
+            text: "All of your data will be deleted.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Log out!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Log out',
+                    'Your data has been deleted.',
+                    'success'
+                )
+                props.setUsername(null);
+            }
+        })
+    }
+    return(
+        <div className={styles.mainContainer}>
+            <div className={styles.maintext}>
+                좋은 {props.dayTime}입니다. {props.username}
+                <button className={styles.logoutBtn} onClick={logout}>Log out</button>
+            </div>
+        </div>
+    )
+}
+function GithubMark() {
+    return(
+        <div className={styles.by}>
+            by <a href={"https://github.com/minseoky"} target="_blank">minseoky<img className={styles.github} src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img></a>
+        </div>
+    )
+}
+function TodoList(){
+    const [newlist, setNewlist] = useState("");
+    const [lists, setLists] = useState([]);
     const onSubmit = (event) => {
         event.preventDefault();
-        if(todo === ""){
-            return;
-        }
-        setTodo("");
-        setTodos((prev) => [...prev,todo]);
+        setLists((prev) => [...prev, newlist])
+        setNewlist("");
     }
-
-
-    const onMouseEnter = () => {
-        setBtnColor("orange");
+    const onChange = (event) => {
+        setNewlist(event.target.value);
     }
-    const onMouseLeave = () => {
-        setBtnColor("tomato");
-    }
-    const todoClick = (event) => {
-        console.log(event.target.innerHTML);
-        const target = event.target.innerHTML;
-        const tempArr = [];
-        let s = 0;
-        for(let i = 0 ; i < todos.length ; i++){
-            if(todos[i] != target){
-                tempArr[s] = todos[i];
-                s++;
-            }
-            else{
-                setDones((prev) => [...prev, todos[i]]);
-            }
-        }
-        setTodos(tempArr);
-    }
-    const doneClick = (event) => {
-        console.log(event.target.innerHTML);
-        const target = event.target.innerHTML;
-        const tempArr = [];
-        let s = 0;
-        for(let i = 0 ; i < dones.length ; i++){
-            if(dones[i] != target){
-                tempArr[s] = dones[i];
-                s++;
-            }
-            else{
-                setTodos((prev) => [...prev, dones[i]]);
-            }
-        }
-        setDones(tempArr);
-    }
-
-    useEffect(() => {
-        console.log(todos);
-    },[todos])
-    function LoginForm(){
-        const [name, setName] = useState("");
-        const changeName = (event) => {
-            setName(event.target.value);
-        }
-        const submitName = (event) => {
-            event.preventDefault();
-            setLoggedin(true);
-            localStorage.setItem("username", name);
-            localStorage.setItem("logged", true);
-        }
-        return(
-            <div className={'login'}>
-                <form onSubmit={submitName}>
-                    <label>good {time}, </label>
-                    <input type="text" value={name} onChange={changeName} className={"login-name"} placeholder={"input your name"}/>
-                </form>
-            </div>
-        )
-    }
-    function AfterLogin() {
-        return(
-            <div className={'afterlogin'}>
-                good {time}, {localStorage.getItem("username")}
-            </div>
-        )
-    }
-    function Clock(){
-        return(
-            <div className={"clock"}>
-                {clock}
-            </div>
-        )
-    }
-    return (
-        <div className={'main-container'} style={{backgroundImage:imgs[randomIndex]}}>
-            <Clock/>
-            <h1 className={'title'}>My To Dos ({todos.length})</h1>
-            <div className={'inputDiv'}>
+    return(
+        <div className={styles.todolist}>
+            <p>To Do List</p>
+            <div className={styles.todolistContainer}>
                 <form onSubmit={onSubmit}>
-                    <input className={'input'} value={todo} onChange={onChange} type="text" placeholder={"write your to do..."}/>
-                    <button onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} style={{backgroundColor: btnColor}} className={'btn'}>submit</button>
+                    <input type={"text"} className={styles.todolistInput} value={newlist} onChange={onChange} placeholder={"write your list"}/>
+                    <ul className={styles.innerList}>
+                        {lists.map((item) => {
+                            return (
+                                <li key={item}><input type={"checkbox"} className={styles.check}/> {item}</li>
+                            )
+                        })}
+                    </ul>
                 </form>
             </div>
-            {loggedin ? <AfterLogin/> : <LoginForm/>}
+        </div>
+    )
+}
+function App() {
+    const [times, setTimes] = useState([new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()]);
+    const [dayTime, setDayTime] = useState("");
+    const [username, setUsername] = useState(null);
+    useEffect(() => {
+        if(localStorage.getItem("username") !== null){}
+            setUsername(localStorage.getItem("username"));
+        }
+    ,[])
 
-            <div className={'list1'} style={{opacity: todos.length == 0 ? 0 : 1}}>
-                <div style={{textAlign : "center"}}>WHAT TO DO</div>
-                <ul>
-                    {todos.map( (item) => {
-                        return (
-                            <li onClick={todoClick} key={item}>{item}</li>
-                        );
-                    })}
-                </ul>
-            </div>
-
-            <div className={'list2'} style={{opacity: dones.length == 0 ? 0 : 1}}>
-                <div style={{textAlign : "center"}}>COMPLETED</div>
-                <ul style={{textDecoration : "line-through"}}>
-                    {dones.map( (item) => {
-                        return (
-                            <li onClick={doneClick} key={item}>{item}</li>
-                        );
-                    })}
-                </ul>
-            </div>
+    return (
+        <div
+            className={styles.background}
+            style={{backgroundImage: bgImgs[bgIndex]}}
+        >
+            <Clock times={times} setTimes={setTimes} dayTime={dayTime} setDayTime={setDayTime}/>
+            {username == null ? <LoginForm dayTime={dayTime} username={username} setUsername={setUsername}/> : <AfterLogin dayTime={dayTime} username={username} setUsername={setUsername}/>}
+            {username == null ? null : <TodoList/>}
+            <GithubMark/>
         </div>
     );
 }
